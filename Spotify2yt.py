cd232feab305df1306a1879aa072d79fef1ab97c
chaseleto <74798031+chaseleto@users.noreply.github.com>
2022-09-06 08:24:33 -0400

    Update Spotify2yt.py

--- Spotify2yt.py
+++ Spotify2yt.py
@@ -12,6 +12,7 @@ def get_spotify_playlist():
     total = int(sp.current_user_saved_tracks()["total"] /rlimit)
     songnames = []
 #test
+#testing
 
     test = 333333333test343242344389889349999
     for x in range(total):
